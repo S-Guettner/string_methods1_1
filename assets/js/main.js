18855a54c1684448b11af469abc91ec0a8c95db5
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-24 12:22:13 +0100

    finish

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+const firstName = "Sven";
+const lastName = "Güttner";
+
+console.log(firstName.length);
+console.log(lastName.length);
+
+let fullName = firstName + lastName;
+console.log(fullName.length);
